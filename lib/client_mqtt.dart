e055416a3a945caa0ef0aa2e84482aa1be765e1f
João Victor <joaoaraujo.367@gmail.com>
2020-04-24 14:58:30 -0300

    Add client mqtt

---
+++ lib/client_mqtt.dart
@@ -0,0 +1,86 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:mqtt_client/mqtt_client.dart';
+import 'package:mqtt_client/mqtt_server_client.dart';
+
+import 'package:notification_app/notification.dart' as notification;
+
+final client = MqttServerClient('server', 'clientID');
+
+Future<void> runMqttServerClient(BuildContext context) async {
+  notification.Notification notify = notification.Notification(context);
+
+  client.autoReconnect = true;
+
+  client.onAutoReconnect = onAutoReconnect;
+
+  client.onConnected = onConnected;
+
+  client.onSubscribed = onSubscribed;
+
+  client.pongCallback = pong;
+
+  final connMess = MqttConnectMessage()
+      .withClientIdentifier('Mqtt_MyClientUniqueId')
+      .keepAliveFor(60)
+      .withWillTopic('willtopic')
+      .withWillMessage('My Will message')
+      .startClean()
+      .withWillQos(MqttQos.atLeastOnce);
+  print('EXAMPLE::Mosquitto client connecting....');
+  client.connectionMessage = connMess;
+
+  try {
+    await client.connect();
+  } on Exception catch (e) {
+    print('EXAMPLE::client exception - $e');
+    client.disconnect();
+  }
+
+  if (client.connectionStatus.state == MqttConnectionState.connected) {
+    print('EXAMPLE::Mosquitto client connected');
+  } else {
+    print(
+        'EXAMPLE::ERROR Mosquitto client connection failed - disconnecting, status is ${client.connectionStatus}');
+    client.disconnect();
+    exit(-1);
+  }
+
+  print('EXAMPLE::Subscribing to the test/lol topic');
+  const topic = 'test/lol';
+  client.subscribe(topic, MqttQos.atMostOnce);
+
+  client.updates.listen((List<MqttReceivedMessage<MqttMessage>> c) {
+    final MqttPublishMessage recMess = c[0].payload;
+    final pt =
+        MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
+
+    print(
+        'EXAMPLE::Change notification:: topic is <${c[0].topic}>, payload is <-- $pt -->');
+    print('');
+    notify.showNotification(c[0].topic, pt);
+  });
+
+  print('EXAMPLE::Sleeping....');
+  await MqttUtilities.asyncSleep(120);
+}
+
+void onSubscribed(String topic) {
+  print('EXAMPLE::Subscription confirmed for topic $topic');
+}
+
+void onAutoReconnect() {
+  print(
+      'EXAMPLE::onAutoReconnect client callback - Client auto reconnection sequence will start');
+}
+
+void onConnected() {
+  print(
+      'EXAMPLE::OnConnected client callback - Client connection was sucessful');
+}
+
+void pong() {
+  print('EXAMPLE::Ping response client callback invoked - you may want to disconnect your broker here');
+}
